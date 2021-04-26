d188cfa54a862e609c42c9850eda73afa01f18d1
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-04-26 19:53:06 +0900

    test

--- server/src/main/java/backend/server/ServerApplication.java
+++ server/src/main/java/backend/server/ServerApplication.java
@@ -2,11 +2,16 @@ package backend.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer {
+
+    @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ServerApplication.class); }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
