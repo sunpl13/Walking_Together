027e5e947bd693ed6ba92f2e3991923b1601a23c
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-05-12 15:28:21 +0900

    CORS setting -14

--- server/src/main/java/backend/server/controller/TestController.java
+++ server/src/main/java/backend/server/controller/TestController.java
@@ -18,7 +18,7 @@ public class TestController {
 
     @GetMapping("/webconfigTest")
     public String webConfigTest() {
-        return "CORS TEST 0512 1시 27분";
+        return "CORS TEST 0512 3시 27분";
     }
 
 }
