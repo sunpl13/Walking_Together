41a3d49d2baa12ec44dcf5a478ae82f9f19d63f9
Jungsoo Kim <sunpl13@naver.com>
2021-06-06 15:55:54 +0900

    서비스워커 다운로드버튼 추가

--- client/public/Install.js
+++ client/public/Install.js
@@ -24,8 +24,10 @@ function installPWA() {
     deferredInstallPrompt.userChoice
     .then((choice) => {
         if(choice.outcome === 'accepted') {
+            alert("설치를 시작하겠습니다.")
             console.log("설치 승인 했을 때", choice);
         } else {
+            alert("설치를 하지 않고 실행 시 앱 사용의 제한을 받을 수 있습니다.")
             console.log("설치 거부 했을 때", choice);
         }
         deferredInstallPrompt = null;
