50791122a9c1da3cce4f9827a0ac0c7659ec596a
shinsojeong <shinsojeong82@gmail.com>
2021-09-03 15:58:42 +0900

    front0903 input function

--- client/src/utils/Function.js
+++ client/src/utils/Function.js
@@ -1,4 +1,5 @@
-//
+import { useState } from 'react';
+
 //파트너 구분 (코드 => 텍스트)
 export const checkPartnerDetail = (detail) => {
     if(detail==="e") {
@@ -12,4 +13,16 @@ export const checkPartnerDetail = (detail) => {
     } else if(detail==="p") {
          return "임산부"
     }
+}
+
+//input state
+export const useInput = (init) => {
+     const [value, setValue] = useState(init);
+     const onChange = (e) => {
+          const {
+               target: {value}
+          } = e;
+          setValue(value);
+     };
+     return { value, onChange };
 }
